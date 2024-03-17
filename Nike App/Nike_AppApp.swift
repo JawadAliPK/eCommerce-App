a3e32b22c43fbbc1996bb6104f4965f425190b48
Jawad Ali <jawad@eddress.co>
2024-03-17 06:10:18 +0100

    Initial Commit

---
+++ Nike App/Nike_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nike_AppApp.swift
+//  Nike App
+//
+//  Created by jawad ali on 17/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Nike_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
